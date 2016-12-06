35f6ff2c7e89cf7d698d5bd9854a82beec0037ab
Kylan Hurt <kylan@datagoniaweb.com>
2016-12-06 01:09:08 -0800

    Initial commit

---
+++ src/constants/LoginConstants.js
@@ -0,0 +1,8 @@
+var BASE_URL = 'http://localhost/';
+export default {
+  BASE_URL: BASE_URL,
+  LOGIN_URL: BASE_URL + 'api/authenticate',
+  SIGNUP_URL: BASE_URL + 'api/user/register',
+  LOGIN_USER: 'LOGIN_USER',
+  LOGOUT_USER: 'LOGOUT_USER'
+}
---
+++ src/services/AuthService.js
@@ -0,0 +1,48 @@
+import request from 'reqwest';
+import when from 'when';
+import {LOGIN_URL, SIGNUP_URL} from '../constants/LoginConstants';
+import LoginActions from '../actions/LoginActions';
+
+class AuthService {
+
+  login(email, password) {
+    return this.handleAuth(when(request({
+      url: LOGIN_URL,
+      method: 'POST',
+      crossOrigin: true,
+      contentType: "application/json",
+      type: 'json',
+       data: JSON.stringify({  
+        email: email, 
+        password: password
+      })
+    })));
+  }
+
+  logout() {
+    LoginActions.logoutUser();
+  }
+
+  signup(email, password, extra) {
+    return this.handleAuth(when(request({
+      url: SIGNUP_URL,
+      method: 'POST',
+      crossOrigin: true,
+      type: 'json',
+      data: {
+        email, password, extra
+      }
+    })).then(this.login(email, password)));
+  }
+
+  handleAuth(loginPromise) {
+    return loginPromise
+      .then(function(response) {
+        var jwt = response.token;
+        LoginActions.loginUser(jwt);
+        return true;
+      });
+  }
+}
+
+export default new AuthService()
