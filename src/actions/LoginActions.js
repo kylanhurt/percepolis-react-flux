439f592d56bf5fe9c6acbc2d49701e344153ee61
kylanhurt <kylan@datagoniaweb.com>
2017-01-27 15:31:26 -0800

    Fix logout issue caused by older react-router version syntax

--- src/actions/LoginActions.js
+++ src/actions/LoginActions.js
@@ -1,8 +1,10 @@
 import AppDispatcher from '../dispatchers/AppDispatcher.js';
 import {LOGIN_USER, LOGOUT_USER} from '../constants/LoginConstants.js';
 import RouterContainer from '../services/RouterContainer'
+import {Router, browserHistory} from 'react-router'
 
 export default {
+
   loginUser: (jwt) => {
     var savedJwt = localStorage.getItem('jwt');  
     AppDispatcher.dispatch({
@@ -13,12 +15,12 @@ export default {
     if (savedJwt !== jwt) {      
       var nextPath = RouterContainer.get().getCurrentQuery().nextPath || '/';
       localStorage.setItem('jwt', jwt);      
-      RouterContainer.get().transitionTo(nextPath);
+      browserHistory.push(nextPath);
     }
   },
   logoutUser: () => {
-    RouterContainer.get().transitionTo('/');
-    localStorage.removeItem('jwt');
+    localStorage.removeItem('jwt');    
+    browserHistory.push('/');
     AppDispatcher.dispatch({
       actionType: LOGOUT_USER
     });
