f69efe8d741c1e28bb2e1b8f8104b75d0c1dda14
kylanhurt <kylan@datagoniaweb.com>
2017-02-08 00:19:28 -0800

    Remove another extraneous log console

--- src/utils/percepolisApi.js
+++ src/utils/percepolisApi.js
@@ -4,8 +4,7 @@ import request from 'superagent';
 export default {
 
   get: function(endpoint, data) {
-  	console.log('inside percApi and data is:', data);
-    request.get('http://localhost/api/' + endpoint).query(data)
+  	  request.get('http://localhost/api/' + endpoint).query(data)
       .set('Accept', 'application/json')
       .end(function(err, response) {
         if (err) return console.error(err);
